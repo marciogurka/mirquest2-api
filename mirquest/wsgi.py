7bc741e220e3c70aa50d81c7245587a4c69cc640
Márcio José Gurka Jr <marciogurkajr@gmail.com>
2019-09-18 15:37:46 -0300

    🐛Fix os to sys python lib usage

--- mirquest/wsgi.py
+++ mirquest/wsgi.py
@@ -8,14 +8,15 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mirquest.settings')
 
 # add the project path into the sys.path
-os.path.append('/var/www/html/mirquest2/mirquest2-api')
+sys.path.append('/var/www/html/mirquest2/mirquest2-api')
 # add the virtualenv site-packages path to the sys.path
-os.path.append('/var/www/html/mirquest2/mirquest2-api/lib/python3.7/site-packages')
+sys.path.append('/var/www/html/mirquest2/mirquest2-api/lib/python3.7/site-packages')
 
 application = get_wsgi_application()
